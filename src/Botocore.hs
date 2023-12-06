5b8d8088c579d902519c3bd48a50d91a11a0cadc
Jack Kelly <jack.kelly@bellroy.com>
2023-12-06 10:56:32 +1000

    botocore: library scaffolding

---
+++ src/Botocore.hs
@@ -0,0 +1,8 @@
+-- |
+-- Module      : Botocore
+-- Copyright   : (c) 2023 Bellroy Pty Ltd
+-- License     : BSD-3-Clause
+-- Maintainer  : Bellroy Tech Team <haskell@bellroy.com>
+-- Stability   : provisional
+-- Portability : non-portable (GHC extensions)
+module Botocore where
