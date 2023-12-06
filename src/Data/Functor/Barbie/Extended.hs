d72a80491f7b1de38344c36b36a3fe0cd2e0c36c
Jack Kelly <jack.kelly@bellroy.com>
2023-12-06 14:32:12 +1000

    botocore: Invent a framework for parsing records from token streams

---
+++ src/Data/Functor/Barbie/Extended.hs
@@ -0,0 +1,17 @@
+-- |
+-- Module      : Data.Functor.Barbie.Extended
+-- Copyright   : (c) 2023 Bellroy Pty Ltd
+-- License     : BSD-3-Clause
+-- Maintainer  : Bellroy Tech Team <haskell@bellroy.com>
+-- Stability   : provisional
+-- Portability : non-portable (GHC extensions)
+module Data.Functor.Barbie.Extended (module Data.Functor.Barbie, bfor) where
+
+import Data.Functor.Barbie
+
+bfor ::
+  (TraversableB b, Applicative e) =>
+  b f ->
+  (forall a. f a -> (e (g a))) ->
+  e (b g)
+bfor b f = btraverse f b
