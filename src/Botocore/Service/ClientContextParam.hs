153da8f02e78b92d0576575cedde99379f3f2fb8
Jack Kelly <jack.kelly@bellroy.com>
2023-12-26 05:43:58 +1000

    botocore: Complete parsing for shapes-2.json

--- src/Botocore/Service/ClientContextParam.hs
+++ src/Botocore/Service/ClientContextParam.hs
@@ -6,27 +6,45 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 -- |
--- Module      : Botocore.Service.Operation.Input.XmlNamespace
+-- Module      : Botocore.Service.ClientContextParam
 -- Copyright   : (c) 2023 Bellroy Pty Ltd
 -- License     : BSD-3-Clause
 -- Maintainer  : Bellroy Tech Team <haskell@bellroy.com>
 -- Stability   : provisional
 -- Portability : non-portable (GHC extensions)
-module Botocore.Service.Operation.Input.XmlNamespace where
+module Botocore.Service.ClientContextParam where
 
 import Barbies (Barbie (..))
 import Barbies.TH (passthroughBareB)
 import Data.Aeson.Decoding.Tokens (Tokens (..))
-import Data.Aeson.Decoding.Tokens.Direct (Parser (..), field, record, text)
+import Data.Aeson.Decoding.Tokens.Direct
+  ( Parser (..),
+    enum,
+    field,
+    record,
+    text,
+  )
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
+data Type = Boolean
+  deriving (Bounded, Enum, Eq, Ord, Show, Generic)
+
 $( passthroughBareB
      [d|
-       data XmlNamespace = XmlNamespace {uri :: Text}
+       data ClientContextParam = ClientContextParam
+         { documentation :: Text,
+           type_ :: Type
+         }
          deriving stock (Eq, Show, Generic)
        |]
  )
 
-parse :: Parser Tokens k e XmlNamespace
-parse = record XmlNamespace {uri = field "uri" text}
+parse :: Parser Tokens k e ClientContextParam
+parse =
+  record
+    ClientContextParam
+      { documentation = field "documentation" text,
+        type_ = field "type" . enum $ \case
+          Boolean -> "boolean"
+      }
---
+++ src/Botocore/Service/Shape/Retryable.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- |
+-- Module      : Botocore.Service.Shape.Retryable
+-- Copyright   : (c) 2023 Bellroy Pty Ltd
+-- License     : BSD-3-Clause
+-- Maintainer  : Bellroy Tech Team <haskell@bellroy.com>
+-- Stability   : provisional
+-- Portability : non-portable (GHC extensions)
+module Botocore.Service.Shape.Retryable where
+
+import Barbies (Barbie (..))
+import Barbies.TH (passthroughBareB)
+import Data.Aeson.Decoding.Tokens (Tokens (..))
+import Data.Aeson.Decoding.Tokens.Direct (Parser, bool, field, record)
+import GHC.Generics (Generic)
+
+$( passthroughBareB
+     [d|
+       data Retryable = Retryable {throttling :: Bool}
+         deriving stock (Eq, Show, Generic)
+       |]
+ )
+
+parse :: Parser Tokens k e Retryable
+parse = record Retryable {throttling = field "throttling" bool}
